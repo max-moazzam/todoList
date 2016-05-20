052be2c76599b75909d072eb1178173ceba1285c
max-moazzam <max.moazzam@gmail.com>
2016-05-19 18:20:34 -0700

    Version 8, user inputs and cleaned up cned up code, some refactoring to simplify events

--- todoList.js
+++ todoList.js
@@ -1,4 +1,5 @@
-//Version 7
+//Version 8
+
 var todoList = {
 	todos: [],
 	displayTodos: function() {
@@ -63,13 +64,33 @@ var todoList = {
 	}
 };
 
-var displayTodosButton = document.getElementById('displayTodosButton');
-var toggleAllButton = document.getElementById('toggleAllButton');
-
-displayTodosButton.addEventListener('click', function(){
-	todoList.displayTodos();
-});
-
-toggleAllButton.addEventListener('click', function(){
-	todoList.toggleAll();
-});
+var handlers = {
+  displayTodos: function() {
+	  todoList.displayTodos();
+  },
+  addTodo: function() {
+    var addTodoTextInput = document.getElementById('addTodoTextInput');
+    todoList.addTodo(addTodoTextInput.value);
+    addTodoTextInput.value = "";
+  },
+  changeTodo: function() {
+    var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+    var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoPositionInput.value = '';
+    changeTodoTextInput.value = '';
+  },
+  deleteTodo: function() {
+    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+    deleteTodoPositionInput.value = '';
+  },
+  toggleCompleted: function() {
+    var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+    todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+    toggleCompletedPositionInput.value = '';
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
+  }
+};
